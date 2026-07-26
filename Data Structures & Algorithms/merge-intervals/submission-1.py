aff64384ce15cced30e41c8fe28bfc0049b4c165
Arthur Miada <miada.arthur@gmail.com>
2026-07-25 22:04:18 -0300

    Add: merge-intervals - submission-1

---
+++ Data Structures & Algorithms/merge-intervals/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: (x[0], x[1]))
+
+        i = 1
+        read_idx = 1
+        write_idx = 0
+        for read_idx in range(len(intervals)):
+            if intervals[write_idx][0] <= intervals[read_idx][1] and intervals[read_idx][0] <= intervals[write_idx][1]:
+                intervals[write_idx][1] = max(intervals[write_idx][1], intervals[read_idx][1])
+            else:
+                write_idx += 1
+                intervals[write_idx] = intervals[read_idx]
+        return intervals[:write_idx+1]
