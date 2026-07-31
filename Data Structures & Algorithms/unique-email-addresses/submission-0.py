85f6df77c2840affa3964c4ae13906c67d17108d
Arthur Miada <miada.arthur@gmail.com>
2026-07-31 11:40:32 -0300

    Add: unique-email-addresses - submission-0

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        sentemails = set()
+        for email in emails:
+            local, domain= email.split("@")
+            local = local.replace(".", "")
+            plus = local.find("+")
+            if plus != -1:    
+                local = local[:plus]
+            sentemails.add(local+domain)
+        return len(sentemails)
